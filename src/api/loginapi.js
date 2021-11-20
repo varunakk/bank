0fc2b1271d1cc0344c6ad56a6ce6b39ff69d125e
varunakk <56592896+varunakk@users.noreply.github.com>
2021-11-20 23:04:20 +0530

    Add files via upload

---
+++ src/api/loginapi.js
@@ -0,0 +1,67 @@
+var express = require("express");
+var app = express(); //include the module .this give us the capability the read the body.
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+//import users from "./../assets/users.json";
+//import users from "./../assets/users.json";
+
+const fs = require('fs');
+var users=[];
+
+//let users = ["vineeta:test123","meenu:test123","pankaj:test123","rakesh:test123"];
+app.post("/Login", function (request, response) {
+  let user1 = request.body.username;
+  let passwd = request.body.password;
+  console.log(user1);
+  if(!user1) {
+      response.status(400).json("enter username");
+// stop further execution in this callback
+      return;
+  }
+  if(!passwd) {
+      response.status(400).json("enter password");
+// stop further execution in this callback
+      return;
+  }
+  fs.readFile('./../assets/users.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log(`Error reading file from disk: ${err}`);
+    } else {
+
+        // parse JSON string to JSON object
+        const databases = JSON.parse(data);
+        const u=databases["users"]
+        
+        for (let index = 0; index < u.length; index++) {
+            const element = u[index]["name"];
+            const e=u[index]["password"];
+            users.push(element+":"+e);
+            
+        }
+        console.log(users);
+        let val = users.includes(user1+":"+passwd);
+        console.log(val);
+        
+          if(val){
+              console.log("success");
+              response.status(200).json("user authenticated");
+        
+              return;
+          } else {
+              response.status(400).json("user does not exist");
+              return;
+          }
+   
+    }
+
+});  
+ 
+    
+});
+app.listen(4000);
